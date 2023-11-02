2f39c5a1ac59ba0b30e5435c9ba23aa3d5ef8b42
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-11-02 14:15:39 +0300

    added blog

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 app_name = "myapp"
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("about", views.about, name="about"),
-    path("blog", views.blog, name="blog"),
+    path('', views.home, name='home'),
+    path('shop', views.shop, name='shop'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
+    path('blog', views.blog, name='blog'),
+    path('cart', views.cart, name='cart'),
+    path('checkout', views.checkout, name='checkout'),
+    path('thankyou', views.thankyou, name='thankyou'),
 ]
